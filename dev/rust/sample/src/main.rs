3a9863e0916bff0e765c6cdd49e13c8c8c62413f
swaroopanand <swaroopanand695@gmail.com>
2022-10-25 22:05:34 +0530

    added all the things

---
+++ dev/rust/sample/src/main.rs
@@ -0,0 +1,7 @@
+mod var;
+
+fn main() {
+    println!("Hello, from main file for first time ");
+    var::run();
+    println!("hello again from main file");
+}
---
+++ dev/rust/sample/src/var.rs
@@ -0,0 +1,4 @@
+pub fn run(){
+    println!("this is from var.rs file");
+}
+
