ac8dd6901345c3cd4db9ebf2f5333ecd76c16aaf
swaroopanand <swaroopanand695@gmail.com>
2022-10-25 21:17:37 +0530

    added notes and cf and dev

---
+++ dev/python/sample/py.py
@@ -0,0 +1,13 @@
+def func(a, b):
+    return a + b
+
+
+a = 49
+b = 51
+
+c = func(a, b)
+anand = 20
+print(c)
+
+
+d = 30 + anand
---
+++ dev/web/sample/index.js
@@ -0,0 +1,11 @@
+let a = 23
+let b = 30
+ 
+function ss(a,b) {
+  return a + b
+}
+
+sum = ss(a, b)
+
+console.log(sum)
+
